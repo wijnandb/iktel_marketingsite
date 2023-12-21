63fddd9b9da3e2cc641b5235cdf3efbab80cb213
wijnandb <wijnandb@retta.nl>
2023-12-21 16:23:09 +0100

    Random image with wrapped text

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,34 +1,82 @@
-var body = document.querySelector('body')
-var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
-var menuContainer = document.querySelector('#main-menu-mobile');
+    var images = [
+        'drie-arm-tablet',
+        'voetbal-sprint',
+        'samen-schilderen',
+        'draaitafel-zwartwit',
+        'tennis-speelster',
+        'drie-armen-fiets-bal',
+        'spelend-balletje',
+        'witte-jongens-grote-voetbal',
+        'samen-koken',
+        'presentie-ipad-buiten',
+        'organisator_in_control',
+        'familie-samen-achter-computer',
+        'tennis',
+        'bordspel',
+        'schilderen',
+        'gezin-ipad',
+        'voetbalster',
+        'stress',
+        'planning',
+        'winst-vieren',
+        'tennis-ongeloofwaardig',
+        'moeder-zoon-computer',
+        'presentatie',
+        'vader-kind-laptop',
+        'zwaardvechter',
+        'twee_met_boeken',
+        'generaties-achter-computer',
+        'winnaars',
+        'gezin-apple-computer-ipad',
+        'toneel',
+        'hoofdpijn-veel-papier',
+        'dubbele-draaitafel',
+        'blije-kinderen-feest',
+        'drie-kinderen',
+        'twee-met-pen-en-voetbal',
+        'factuur',
+        'muziekles-dubbele-piano',
+        'estafette-stok',
+        'gamen-met-guns',
+        'duo-schilderen-tulband-baard',
+        'overwhelmed',
+        'poppenspelers',
+        'moeder-dochter-computer',
+        'twee-op-toetsenbord',
+        'stapels-stress'
+    ];
 
-menuTrigger.onclick = function() {
-    menuContainer.classList.toggle('open');
-    menuTrigger.classList.toggle('is-active')
-    body.classList.toggle('lock-scroll')
-}
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('.dropdown').forEach(function(dropdownToggle) {
-      dropdownToggle.addEventListener('click', function(e) {
-        e.preventDefault();
-        let dropdownMenu = this.querySelector('.dropdown-menu');
-        if (dropdownMenu) {
-          dropdownMenu.classList.toggle('show');
-        }
-      });
-    });
+    window.onload = function() {
   
-    window.onclick = function(event) {
-      if (!event.target.matches('.dropdown-toggle')) {
-        var dropdowns = document.getElementsByClassName('dropdown-menu');
-        for (var i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
+      var imageIndex = Math.floor(Math.random() * images.length); // Random index based on the array length
+      var baseurl = "/bredeschool_software"; 
+      var imagePath = baseurl + '/images/illustrations/' + images[imageIndex]; // Using the filename from the array
+  
+      var pictureElement = document.getElementById('random-image');
+      if (pictureElement) {
+          // Clear existing content
+          pictureElement.innerHTML = '';
+  
+          // Create webp source element
+          var sourceWebp = document.createElement('source');
+          sourceWebp.setAttribute('src', imagePath + '.webp');
+          sourceWebp.setAttribute('type', 'image/webp');
+          pictureElement.appendChild(sourceWebp);
+  
+          // Create png source element
+          var sourcePng = document.createElement('source');
+          sourcePng.setAttribute('src', imagePath + '.png');
+          sourcePng.setAttribute('type', 'image/png');
+          pictureElement.appendChild(sourcePng);
+  
+          // Create img element
+          var imgElement = document.createElement('img');
+          imgElement.setAttribute('src', imagePath + '.png');
+          imgElement.setAttribute('alt', 'Random Image');
+          imgElement.classList.add('float-right', 'ml-3', 'mb-3');
+          pictureElement.appendChild(imgElement);
+      } else {
+          console.error('Element with id "random-image" not found.');
       }
-    }
-  });
+  };
   
